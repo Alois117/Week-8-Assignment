f72a07ac5618c5ab11c6c21b1c1b3f49de4b0728
Alois117 <aloismbithi01@gmail.com>
2025-05-02 14:18:38 +0300

    Initial Commit

---
+++ contact-book-api/app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import models, schemas, crud
+from app.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/contacts/", response_model=schemas.Contact)
+def create_contact(contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    return crud.create_contact(db, contact)
+
+@app.get("/contacts/", response_model=list[schemas.Contact])
+def read_contacts(db: Session = Depends(get_db)):
+    return crud.get_contacts(db)
+
+@app.get("/contacts/{contact_id}", response_model=schemas.Contact)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.get_contact(db, contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.put("/contacts/{contact_id}", response_model=schemas.Contact)
+def update_contact(contact_id: int, contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    return crud.update_contact(db, contact_id, contact)
+
+@app.delete("/contacts/{contact_id}")
+def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    return crud.delete_contact(db, contact_id)
